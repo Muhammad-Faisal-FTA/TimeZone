bd481f0f6ec9d3c5914c5f4053cab9a95fa533e5
Muhammad-Faisal_FTA <mfaisal1947fort@gmail.com>
2025-07-14 06:05:10 +0500

    project basic setup is compelete, tailWind css is configured.

--- vite.config.js
+++ vite.config.js
@@ -9,10 +9,15 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'
+
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),    
+    tailwindcss()
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'), // 👈 This defines @ as src
